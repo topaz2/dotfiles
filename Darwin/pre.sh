27e07857aba87df808c0f5c567d285216d668584
topaz2 <topaz2.3333@gmail.com>
2016-02-05 16:37:37 +0900

    Add install scripts for osx

---
+++ Darwin/pre.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+brew install \
+     aircrack-ng axel brew-gem colordiff ctags \
+     emacs \
+     global --with-exuberant-ctags --with-pygments \
+     ispell keepassx lv libffi mysql nkf nodejs \
+     pbzip2 postgresql python \
+     rbenv ruby ruby-build \
+     s3cmd svn transmission tree \
+     w3m wget homebrew/php/php55
+brew cask install \
+     chefdk karabiner mono-mdk \
+     gimp google-chrome google-drive google-japanese-ime \
+     packer vagrant virtualbox
