3f42be6f89749c8b8c87a32959a22934c7e88142
topaz2 <topaz2.3333@gmail.com>
2018-07-18 15:04:59 +0900

    Introduce Brewfile*

--- home/Darwin/pre.sh
+++ home/Darwin/pre.sh
@@ -1,24 +1,8 @@
 #!/bin/zsh
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install \
-     aircrack-ng awscli axel \
-     brew-gem colordiff ctags \
-     docker docker-compose \
-     emacs \
-     global --with-exuberant-ctags --with-pygments \
-     ispell lv screen libffi mysql nkf nodejs \
-     pbzip2 postgresql python \
-     rbenv ruby ruby-build \
-     transmission tree \
-     w3m wget
-brew cask install \
-     dropbox docker \
-     keepassx mono-mdk \
-     gimp google-chrome google-drive-file-stream google-japanese-ime \
-     karabiner-elements seil \
-     mysqlworkbench \
-     slack \
-     virtualbox virtualbox-extension-pack
+
+xargs brew install < Brewfile
+xargs brew cask install < Brewfile.cask
 brew link docker docker-compose
 sudo systemsetup -setremotelogin on
