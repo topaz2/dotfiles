3881014eb6bb31ff0e58cd6eb6b3225bff6f4621
topaz2 <topaz2.3333@gmail.com>
2018-08-08 11:08:16 +0900

    remove package archive

--- home/.emacs.d/elpa/ac-ispell-20131125.1344/ac-ispell-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ac-ispell" "20131125.1344" "ispell completion source for auto-complete" (quote ((auto-complete "1.4"))))
--- home/.emacs.d/elpa/tumble-20130326.2210/tumble-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "tumble" "20130326.2210" "an Tumblr mode for Emacs" (quote ((http-post-simple "0"))))
